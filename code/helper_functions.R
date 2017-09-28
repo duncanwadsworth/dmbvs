1f596fd2ffe4dfaf5a115ff92019c81653a6dad4
Michele Guindani <micheleguindani@gmail.com>
2017-09-28 16:30:55 -0700

    Update helper_functions.R (#1)
    * Update helper_functions.R
    
    See my email
    
    * Update BFDR function

--- code/helper_functions.R
+++ code/helper_functions.R
@@ -13,6 +13,7 @@
 #' method. Biostatistics, 5(2), 155-76. doi:10.1093/biostatistics/5.2.155
 #'
 #' @export
+
 bfdr = function(mppi_vector, threshold = 0.1){
   # arg checking
   if(any(mppi_vector > 1 | mppi_vector < 0)){
@@ -21,17 +22,15 @@ bfdr = function(mppi_vector, threshold = 0.1){
   if(threshold > 1 | threshold < 0){
     stop("Bad input: threshold should be a probability")
   }
-  # pretty simple
-  onek = ((1 - mppi_vector) < threshold) + 0
-  # possible to select none
-  if(sum(onek) == 0){
-    selected = rep(FALSE, length(onek))
-    thecut = 0
-  }else{
-    thecut = sum((1 - mppi_vector) * onek)/sum(onek)
-    selected = (1 - mppi_vector) < thecut
-  }
-  return(list(selected = selected, threshold = 1 - thecut))
+  # sorting the ppi's in decresing order
+  sorted_mppi_vector = sort(mppi_vector, decreasing = TRUE)
+  # computing the fdr 
+  fdr = cumsum((1 - sorted_mppi_vector))/seq(1:length(mppi_vector))
+  # determine index of the largest fdr less than threshold
+  thecut.index = max(which(fdr < threshold))
+  ppi_threshold = sorted_mppi_vector[thecut.index]
+  selected = mppi_vector > ppi_threshold
+  return(list(selected = selected, threshold = ppi_threshold))
 }
 
 #' a bipartite graph showing the association between X and Y

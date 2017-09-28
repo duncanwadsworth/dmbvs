518639f10027fa098f319759627c2687105faca0
duncanwadsworth <duncanwadsworth@users.noreply.github.com>
2017-09-28 16:27:20 -0700

    Update BFDR function

--- code/helper_functions.R
+++ code/helper_functions.R
@@ -23,11 +23,11 @@ bfdr = function(mppi_vector, threshold = 0.1){
     stop("Bad input: threshold should be a probability")
   }
   # sorting the ppi's in decresing order
-  sorted_mppi_vector=sort(mppi_vector, decreasing=TRUE)
+  sorted_mppi_vector = sort(mppi_vector, decreasing = TRUE)
   # computing the fdr 
-  fdr <- cumsum((1-sorted_mppi_vector))/seq(1:length(mppi_vector))
+  fdr = cumsum((1 - sorted_mppi_vector))/seq(1:length(mppi_vector))
   # determine index of the largest fdr less than threshold
-  thecut.index=max(which(fdr<threshold))
+  thecut.index = max(which(fdr < threshold))
   ppi_threshold = sorted_mppi_vector[thecut.index]
   selected = mppi_vector > ppi_threshold
   return(list(selected = selected, threshold = ppi_threshold))
